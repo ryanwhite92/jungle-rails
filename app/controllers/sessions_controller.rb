fecc9f68fd806704b1e2c96040751fcc8829f9a8
Ryan White <ryanjwhite10@gmail.com>
2018-08-16 22:33:34 +0000

    Fix flash notice bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      flash[:notice] = 'Invalid login credentials'
+      flash[:invalid] = 'Invalid login credentials'
       render 'new'
     end
   end
