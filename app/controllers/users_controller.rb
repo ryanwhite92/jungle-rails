ea1ce08518e18ffccef93956424cba505bf090c0
Ryan White <ryanjwhite10@gmail.com>
2018-08-16 18:31:42 +0000

    Add logic to users#create; sanitize input in user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = user_id
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
