fc37ea2a1d604336bb18b0b50e844cf3215dd69b
Ryan White <ryanjwhite10@gmail.com>
2018-08-17 18:55:25 +0000

    Add model validations and associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates_associated :user
+  validates_associated :product
+  validates :description, presence: true
+  validates :rating, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 1,
+                                    less_than_or_equal_to: 5 }
 end
