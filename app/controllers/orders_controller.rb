bd872c62081c5db940380c7bd7a4515cc9cf47d8
Ryan White <ryanjwhite10@gmail.com>
2018-08-17 00:28:49 +0000

    Send email on order confirmation and fix small bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
+      OrderMailer.email_receipt(order).deliver_now
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
