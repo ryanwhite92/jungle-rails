280eb8687270e867f637212089158e9b88e4466b
Ryan White <ryanjwhite10@gmail.com>
2018-08-18 00:49:48 +0000

    Add review controller and update product controller/view

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @reviews = Review.where(product_id: params[:product_id])
+
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render 'products/show'
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :description)
+  end
+end
