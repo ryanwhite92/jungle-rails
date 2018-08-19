685d861acbe3ed03faaff66ec75a6a3837f53989
Ryan White <ryanjwhite10@gmail.com>
2018-08-19 00:04:19 +0000

    Check that registration email is unique and perform simple validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_secure_password
 
@@ -6,6 +7,9 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 
 end
