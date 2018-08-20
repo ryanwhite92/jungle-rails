929b7b0751f3700533c4883c65e9a2df1db401c6
Ryan White <ryanjwhite10@gmail.com>
2018-08-20 18:29:58 +0000

    Refactor validation specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    let(:category) { Category.new(name: 'Drones') }
+    let(:product) { Product.new(name: 'Stealth Drone',
+                                price: 1200,
+                                quantity: 8,
+                                category: category) }
+
     it 'is valid with valid attributes' do
-      category = Category.new(name: 'Drones')
-      product = Product.new(name: 'Stealth Drone', price: 120000, quantity: 8, category: category)
       expect(product).to be_valid
     end
 
     it 'is not valid without a name' do
-      category = Category.new(name: 'Drones')
-      product = Product.new(name: nil, price: 120000, quantity: 8, category: category)
+      product.name = nil
       expect(product).to_not be_valid
     end
 
     it 'is not valid without a price' do
-      category = Category.new(name: 'Drones')
-      product = Product.new(name: 'Stealth Drone', price: nil, quantity: 8, category: category)
+      product.price_cents = nil
       expect(product).to_not be_valid
     end
 
     it 'is not valid without a quantity' do
-      category = Category.new(name: 'Drones')
-      product = Product.new(name: 'Stealth Drone', price: 120000, quantity: nil, category: category)
+      product.quantity = nil
       expect(product).to_not be_valid
     end
 
     it 'is not valid without a category' do
-      category = Category.new(name: 'Drones')
-      product = Product.new(name: 'Stealth Drone', price: 120000, quantity: 8, category: nil)
+      product.category = nil
       expect(product).to_not be_valid
     end
   end
