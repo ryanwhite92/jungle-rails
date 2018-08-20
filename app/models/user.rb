dcc815c2662209f004a5935c9e29f04a676b9fbf
Ryan White <ryanjwhite10@gmail.com>
2018-08-20 21:01:29 +0000

    Add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,7 @@ class User < ActiveRecord::Base
             presence: true,
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 
 end
