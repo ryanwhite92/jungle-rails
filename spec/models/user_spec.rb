363948c82836fd7d037a16181b422480b4c2702b
Ryan White <ryanjwhite10@gmail.com>
2018-08-20 21:31:11 +0000

    Add email validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    let (:user) { User.new(first_name: 'Elliot', last_name: 'Alderson', email: 'alderson@fsociety.com') }
+    let (:user) { User.new(first_name: 'Elliot',
+                           last_name: 'Alderson',
+                           email: 'alderson@fsociety.com') }
 
     context 'must be created with password and password_confirmation fields' do
       it 'should return error message if password is empty' do
@@ -23,6 +25,28 @@ RSpec.describe User, type: :model do
       it 'should not be equal if password and password_confirmation do not match' do
         user.update(password: 'robot', password_confirmation: 'mrrobot')
         expect(user.password).to_not eql(user.password_confirmation)
+      end
+    end
+
+    context 'must be created with a unique email' do
+      let (:user2) { User.new(first_name: 'Darlene',
+                              last_name: 'Alderson',
+                              password: 'mrrobot',
+                              password_confirmation: 'mrrobot') }
+
+      it 'should be invalid if email field is blank' do
+        user2.save
+        expect(user2).to_not be_valid
+      end
+
+      it 'should be invalid if registering with non-unique email' do
+        user.update(email: 'alderson@fsociety.com')
+        expect(user2).to_not be_valid
+      end
+
+      it 'should be case sensitive' do
+        user2.update(email: 'ALDERSON@fsociety.com')
+        expect(user.email).to_not eql(user2.email)
       end
     end
   end
