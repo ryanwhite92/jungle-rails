6acd19edf96fd259492050fce9ff5b4b95b47bae
Ryan White <ryanjwhite10@gmail.com>
2018-08-20 19:31:56 +0000

    Change product validation specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,33 +3,50 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     let(:category) { Category.new(name: 'Drones') }
-    let(:product) { Product.new(name: 'Stealth Drone',
-                                price: 1200,
-                                quantity: 8,
-                                category: category) }
+    let!(:product) { Product.new(name: 'Stealth Drone',
+                                 price: 1200,
+                                 quantity: 8,
+                                 category: category) }
 
-    it 'is valid with valid attributes' do
-      expect(product).to be_valid
+    context 'with valid attributes' do
+      it 'should be valid' do
+        expect(product).to be_valid
+      end
+
+      it 'should save product to database' do
+        product.save
+        expect(product.errors.full_messages.length).to eql(0)
+      end
     end
 
-    it 'is not valid without a name' do
-      product.name = nil
-      expect(product).to_not be_valid
+    context 'without a valid name' do
+      before { product.update(name: nil) }
+
+      it 'should return a name error message' do
+        expect(product.errors.full_messages.first).to eql('Name can\'t be blank')
+      end
     end
 
-    it 'is not valid without a price' do
-      product.price_cents = nil
-      expect(product).to_not be_valid
+    context 'without a valid price' do
+      before { product.update(price_cents: nil) }
+
+      it 'should return a price error message' do
+        expect(product.errors.full_messages.first).to eql('Price cents is not a number')
+      end
     end
 
-    it 'is not valid without a quantity' do
-      product.quantity = nil
-      expect(product).to_not be_valid
+    context 'without a valid quantity' do
+      it 'should return a quantity error message' do
+        product.update(quantity: nil)
+        expect(product.errors.full_messages.first).to eql('Quantity can\'t be blank')
+      end
     end
 
-    it 'is not valid without a category' do
-      product.category = nil
-      expect(product).to_not be_valid
+    context 'without a valid category' do
+      it 'should return a category error message' do
+        product.update(category: nil)
+        expect(product.errors.full_messages.first).to eql('Category can\'t be blank')
+      end
     end
   end
 end
