74284dfd9cc63371a662a6be01c7f1d7bd05bdc5
Ryan White <ryanjwhite10@gmail.com>
2018-08-21 16:04:02 +0000

    Refactor specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,38 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe 'After creation' do
-    before :each do
-      drones = Category.new(name: 'Drones')
-      @product1 = Product.create!(id: 1, name: 'Stealth Drone', price: 1500, quantity: 8, category: drones)
-      @product2 = Product.create!(id: 2, name: 'Camera Drone', price: 800, quantity: 22, category: drones)
-      @product3 = Product.create!(id: 3, name: 'Mini Drone', price: 500, quantity: 18, category: drones)
-    end
+    let(:drones) { Category.new(name: 'Drones') }
+    let(:product1) { Product.create!(id: 1, name: 'Stealth Drone', price: 1500, quantity: 8, category: drones) }
+    let(:product2) { Product.create!(id: 2, name: 'Camera Drone', price: 800, quantity: 22, category: drones) }
+    let(:product3) { Product.create!(id: 3, name: 'Mini Drone', price: 500, quantity: 18, category: drones) }
 
-    it 'deducts quantity from products based on their line item quantities' do
+    before :each do
       @order = Order.new(stripe_charge_id: 1)
-      @order.line_items.new(product: @product1, quantity: 1, item_price: @product1.price, total_price: @product1.price * 1)
-      @order.line_items.new(product: @product2, quantity: 2, item_price: @product2.price, total_price: @product2.price * 2)
+      @order.line_items.new(product: product1, quantity: 1, item_price: product1.price, total_price: product1.price * 1)
+      @order.line_items.new(product: product2, quantity: 2, item_price: product2.price, total_price: product2.price * 2)
       @order.total_cents = @order.line_items.reduce(0) { |sum, i| sum + i.total_price }
 
       @order.save!
-      @product1.reload
-      @product2.reload
+    end
 
-      expect(@product1.quantity).to eql(7)
-      expect(@product2.quantity).to eql(20)
+    it 'deducts quantity from products based on their line item quantities' do
+      product1.reload
+      product2.reload
+
+      expect(product1.quantity).to eql(7)
+      expect(product2.quantity).to eql(20)
 
     end
 
     it 'does not deduct quantity from products that are not in the order' do
-      @order = Order.new(email: 'email@example.com', stripe_charge_id: '1')
-      @order.line_items.new(product: @product1, quantity: 1, item_price: @product1.price, total_price: @product1.price * 1)
-      @order.line_items.new(product: @product2, quantity: 2, item_price: @product2.price, total_price: @product2.price * 2)
-      @order.total_cents = @order.line_items.reduce(0) { |sum, i| sum + i.total_price }
+      product3.reload
 
-      @order.save!
-      @product3.reload
-
-      expect(@product3.quantity).to eql(18)
+      expect(product3.quantity).to eql(18)
     end
   end
 end
