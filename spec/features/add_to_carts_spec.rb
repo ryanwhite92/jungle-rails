fac8c6f2028d31d1953c45fd60cafa79704b8c70
Ryan White <ryanjwhite10@gmail.com>
2018-08-21 20:51:37 +0000

    Setup add to cart feature specs

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Users click add to cart button", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'Cart increases by one' do
+    visit root_path
+
+    save_screenshot 'output.png'
+  end
+
+end
