f446d0d4903696c4bab70f90cc8e86493e0640ea
Ryan White <ryanjwhite10@gmail.com>
2018-08-21 14:55:36 +0000

    Adjust product quantities after order is successfully placed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
 
+  after_create { |order| adjust_quantity(order) }
+
   belongs_to :user
   has_many :line_items
 
@@ -7,4 +9,12 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  private
+  def adjust_quantity(order)
+    order.line_items.each do |item|
+      product = Product.find_by(id: item.product.id)
+      quantity = product.quantity - item.quantity
+      product.update(quantity: quantity)
+    end
+  end
 end
