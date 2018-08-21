92d8170b19d4203054a217fc2a16d8ed06665106
Ryan White <ryanjwhite10@gmail.com>
2018-08-21 00:24:21 +0000

    Add authentication class method to user model and update session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,18 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email,
             presence: true,
-            uniqueness: { case_sensitive: false },
-            format: { with: VALID_EMAIL_REGEX }
+            uniqueness: { case_sensitive: false }
+            # format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
+  private
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
